a76eba712688a422b1ba2c9aaf7f493f84dd6418
Si Min Nicole Sim <s10196790@connect.np.edu.sg>
2020-05-10 02:01:42 +0800

    weeek3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,6 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.widget.VideoView;
+import android.net.Uri;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer media =MediaPlayer.create(this,
+                R.raw.baby_laughing);
+        media.start();;
+
+        VideoView rabbidvideo = findViewById(R.id.rabbidvideo);
+        rabbidvideo.setVideoURI(
+                Uri.parse("android.resource://"+getPackageName() + "/" + R.raw.rabbid)
+
+        );
+        rabbidvideo.start();
     }
 }
